6f90537a5c4f6d9a2f13ba5f867d72028670fa1b
macstudent <bushairkm007@gmail.com>
2019-09-20 20:10:36 -0400

    reverse corrected

--- src/main/java/com/lambton/StringHandling.java
+++ src/main/java/com/lambton/StringHandling.java
@@ -1,81 +1,61 @@
 package com.lambton;
 
-//import com.sun.org.apache.xpath.internal.operations.String;
 
 public class StringHandling
 {
-    public static void main(String[]args)
+    public static void main(String args[])
     {
-        String s1="HELLO";
-        String s2="HELLO";
+        String s1="Hello";
+        String s2="Hello";
         String s3=s1;
-
-
-         String s4= new String("HELLO");
-         if(s1==s2)
-         {
-             System.out.println("s1==s2");
-         }else
-         {
-             System.out.println("s1!=s2");
-         }
-         if(s3==s2)
-         {
-             System.out.println("s3=s2");
-         }else
-         {
-             System.out.println("s3!=s2");
-         }
+        String s4= new String("Hello");
+        if(s1==s2)
+        {
+            System.out.println("s1=s2");
+        }
+        else
+        {
+            System.out.println("s1!=s2");
+        }
+        if(s3==s2)
+        {
+            System.out.println("s3=s2");
+        }
+        else
+        {
+            System.out.println("s3!=s2");
+        }
         if(s1==s4)
         {
             System.out.println("s1=s4");
-        }else
+        }
+        else
         {
             System.out.println("s1!=s4");
         }
-        if (s1.equals(s4))
-        {
-            System.out.println("s1==s4");
-        }
         String collegeName="Lambton college in Toronto";
-        collegeName.length();
-        collegeName.contains("college");
-        collegeName.indexOf("Toronto");
-        collegeName.toUpperCase();
-        collegeName.toLowerCase();
-        collegeName.charAt(10);
-        collegeName.replace("Toronto","New York");
-        //collegeName.substring();
-        collegeName.toCharArray();
-        collegeName.concat("265 Yorkland Blvd.,Northyork");
-
-        System.out.println("Length is:"+collegeName.length());
-        System.out.println("Contains:"+collegeName.contains("college"));
-        System.out.println("Index of Toronto is:"+collegeName.indexOf("Toronto"));
-        System.out.println("Upper case is:"+collegeName.toUpperCase());
-        System.out.println("Index of Toronto is:"+collegeName.toLowerCase());
-        System.out.println("Replaced:"+collegeName.replace("Toronto","New York"));
-
-        boolean b=collegeName.contains("college");
+        System.out.println("length :"+collegeName.length());
+        System.out.println("Uppercase :"+collegeName.toUpperCase());
+        System.out.println("Lowercase :"+collegeName.toLowerCase());
+        System.out.println("Character at index 10 :"+collegeName.charAt(10));
+        System.out.println("Replace :"+collegeName.replace("Toronto","Newyork"));
+        System.out.println("to character array :"+collegeName.toCharArray());
+        System.out.println("substring :"+collegeName.substring(11,20));
+        Boolean b=collegeName.contains("college");
         System.out.println(b);
-
-
-        char c[] =collegeName.toCharArray();
-
-        for(int i=0;i<c.length;i++)
-        {
-            System.out.print(c[i]);
-        }
-
+        char c[]=collegeName.toCharArray();
         char rev[]=new char[c.length];
         int cnt=0;
         collegeName.length();
+        for(int i=0;i<c.length;i++)
+        {
+            System.out.println(c[i]);
+        }
         for(int i=c.length-1;i>=0;i--)
         {
-           rev[cnt++]=c[i];
-           String reversestring=new String(rev);
-           System.out.println("REVERSE IS"+reversestring);
+            rev[cnt++] = c[i];
         }
-
+        String reverseString=new String(rev);
+        System.out.println("Reverse String = " + reverseString);
     }
 }
