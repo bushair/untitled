3faec22ea42d62cb489748710d4b495a32603644
macstudent <bushairkm007@gmail.com>
2019-09-20 20:52:43 -0400

    Age By DOB

--- src/main/java/com/lambton/StringHandling.java
+++ src/main/java/com/lambton/StringHandling.java
@@ -1,11 +1,55 @@
 package com.lambton;
 
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.TimeZone;
+
 public class StringHandling
 {
     public static void main(String args[])
     {
-        String s1="Hello";
+
+        // date of birth
+        LocalDate pdate = LocalDate.of(1989, 04, 11);
+        // current date
+        LocalDate now = LocalDate.now();
+        // difference between current date and date of birth
+        Period diff = Period.between(pdate, now);
+
+        System.out.printf("\nI am  %d years, %d months and %d days old.\n\n",
+                diff.getYears(), diff.getMonths(), diff.getDays());
+
+        
+        
+        //date and time
+
+        SimpleDateFormat dateTimeInGMT = new SimpleDateFormat("dd-MM-yy hh:mm:ss aa");
+        //Setting the time zone
+        dateTimeInGMT.setTimeZone(TimeZone.getTimeZone("GMT"));
+        System.out.println(dateTimeInGMT.format(new Date()));
+
+       // System.out.println(localDateTime.now());
+
+
+      /*  String pattern="###,###,##";
+        DecimalFormat decimalFormat=new DecimalFormat();
+       decimalFormat.applyPattern(pattern);
+       int value=12345678;
+       String output=decimalFormat.format(value);
+       System.out.println(value+""+output);
+
+
+
+       String s=String.format("%d %s %s,",10,"Hello World");
+        System.out.print(s);
+
+       String s1="Hello";
         String s2="Hello";
         String s3=s1;
         String s4= new String("Hello");
@@ -43,6 +87,8 @@ public class StringHandling
         System.out.println("substring :"+collegeName.substring(11,20));
         Boolean b=collegeName.contains("college");
         System.out.println(b);
+
+        //reversing
         char c[]=collegeName.toCharArray();
         char rev[]=new char[c.length];
         int cnt=0;
@@ -57,5 +103,9 @@ public class StringHandling
         }
         String reverseString=new String(rev);
         System.out.println("Reverse String = " + reverseString);
+        integer to string
+        */
+
+
     }
 }
